51b7c6722be72e0cabf674aa0a033f0b21ab77a9
Steven <34237303+Keritzy@users.noreply.github.com>
2022-06-06 22:17:31 +0100

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,10 @@
+def triangle():
+    enter1=input("Type Height >")
+    enter2=input("Type Base >")
+    h=int(enter1)
+    b=int(enter2)
+    area=(b*h)/2
+    print("Area of triangle "+str(area))   #Display Area
+ 
+ 
+triangle()    #run function which is triangle 
