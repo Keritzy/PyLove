ebe8d49048588b197aff7aa5df3dc0b8854fe73a
Steven <34237303+Keritzy@users.noreply.github.com>
2022-07-03 19:28:54 +0100

    Add files via upload

---
+++ looptest.py
@@ -0,0 +1,25 @@
+mynameissteven = ['my', 'name', 'is', 'steven']
+for i in mynameissteven:
+    print(i)
+
+for i in mynameissteven:
+    print(i)
+    print(i)
+
+for i in mynameissteven:
+    print(i)
+    for x in mynameissteven:
+        print(x)
+    print('one of it is accomplished')
+
+
+for what in range(0, 10 + 1):
+    print(what)
+
+x = 100
+y = 200
+
+while x < 140 and y < 200:
+    x = x + 1
+    y = y + 1
+    print(x, y)
