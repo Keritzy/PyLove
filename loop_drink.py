07d52729f201bf6d103113bead77939096778c73
Steven <34237303+Keritzy@users.noreply.github.com>
2022-07-03 19:36:05 +0100

    Add files via upload

---
+++ loop_drink.py
@@ -0,0 +1,17 @@
+myfavalchol = [
+    'Soju',
+    'Beer',
+    'Whiskey',
+    'Gin',
+    'Wine',
+    'Vodka'
+]
+
+
+
+# value null we would put print(myfavalchol[0])
+
+numerical = [1,2,3,4,5,6]
+for x in numerical:
+    print(str(x) + " " + myfavalchol[x - 1])
+    
